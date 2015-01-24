0814ef99f887ff1b4ee14fa81588383185351460
rainbee2214 <rainbee2214@gmail.com>
2015-01-23 21:48:24 -0400

    Added a walking right anomation.

--- GlobalGameJam/Assets/Scripts/PlayerController.cs
+++ GlobalGameJam/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(PlayerPhysics))]
 public class PlayerController : MonoBehaviour
 {
+    PlayerPhysics playerPhysics;
 
-    void Start()
+    void Awake()
     {
-
+        playerPhysics = GetComponent<PlayerPhysics>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0 ||
+            Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0) playerPhysics.Move();
 
+        if (Input.GetButtonDown("Jump")) playerPhysics.Jump();
+
+        playerPhysics.IsGrounded();
     }
 }
--- GlobalGameJam/Assets/Scripts/PlayerPhysics.cs
+++ GlobalGameJam/Assets/Scripts/PlayerPhysics.cs
@@ -3,14 +3,60 @@ using System.Collections;
 
 public class PlayerPhysics : MonoBehaviour
 {
+    string layer1 = "Enemies";
+    string layer2 = "Ground";
 
-    void Start()
+    Vector2 position;
+    float speed = 1f;
+
+    bool grounded = true;
+    float jumpDistance = 2f;
+
+    public bool Grounded
     {
-
+        get { return grounded; }
+        set { grounded = value; }
     }
 
-    void Update()
+    public void Move()
     {
+        position = transform.position;
+
+        position.x += Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+
+        transform.position = position;
+    }
+
+    public void Jump()
+    {
+        if (grounded)
+        {
+            //Jumping logic
+            transform.Translate(new Vector3(0f, jumpDistance, 0f));
+            Debug.Log("Jump!");
+        }
+        else
+        {
+            Debug.Log("Can't jump.");
+        }
+    }
+
+    public void IsGrounded()
+    {
+        Debug.DrawRay(transform.position, -Vector2.up*2.25f, Color.white);
+
+        int layerMask = 1 << LayerMask.NameToLayer(layer1) | 1 << LayerMask.NameToLayer(layer2);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.55f, layerMask);
+        if (hit.collider != null)
+        {
+            Debug.Log("Ray is hitting something.");
+            grounded = true;
+        }
+        else
+        {
+            //Debug.Log("Ray is not hitting something.");
+            grounded = false;
+        }
 
     }
 }
