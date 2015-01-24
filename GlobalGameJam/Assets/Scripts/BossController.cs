4e600cc9db8b4fe6d1658426cd0199a89a83115d
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-01-24 12:45:20 -0400

    Add boss controller, move structs/enums to seperate file

---
+++ GlobalGameJam/Assets/Scripts/BossController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossController : MonoBehaviour 
+{
+	// Reference to main boss
+	// References to mini bosses
+	
+	void removeMainBossResistance(ElementType toRemove)
+	{
+		switch(toRemove)
+		{
+		case ElementType.Fire:
+			//boss.removeResistance(ElementType.Fire);
+			break;
+		case ElementType.Magic:
+			//boss.removeResistance(ElementType.Magic);
+			break;
+		case ElementType.Water:
+			//boss.removeResistance(ElementType.Water);
+			break;
+		}
+	}	
+}
