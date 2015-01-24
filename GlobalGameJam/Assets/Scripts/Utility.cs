6251ce92ebb25b3a0f49996812c0321bc29997b1
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-01-24 14:40:32 -0400

    Add documentation to utility

--- GlobalGameJam/Assets/Scripts/Utility.cs
+++ GlobalGameJam/Assets/Scripts/Utility.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public enum ElementType
 {
@@ -8,14 +9,34 @@ public enum ElementType
 	Magic
 };
 
+/// <summary>
+/// Element stat, used for attack type and value
+/// </summary>
 public struct ElementStat 
 {
 	public ElementType type;
 	public int value;
 }
 
+/// <summary>
+/// Buff stat, used for resistance/weakness type and value
+/// </summary>
 public struct BuffStat
 {
 	public ElementType type;
 	public float value;
+}
+
+public static class Utility
+{
+	/// <summary>
+	/// Gets a random enum from the specified generic
+	/// </summary>
+	/// <typeparam name="T">The enum type to get from.</typeparam>
+	public static T GetRandomEnum<T>()
+	{
+		System.Array A = System.Enum.GetValues(typeof(T));
+		T V = (T)A.GetValue(UnityEngine.Random.Range(0,A.Length));
+		return V;
+	}
 }
