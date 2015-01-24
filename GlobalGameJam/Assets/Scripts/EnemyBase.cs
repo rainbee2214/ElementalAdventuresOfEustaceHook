0e9a6b4be28e6dbe7fd508cb0f689ea4b0f47b5a
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-01-23 20:37:10 -0400

    Finish enemy base class

---
+++ GlobalGameJam/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ElementType
+{
+	Fire,
+	Water,
+	Magic
+};
+
+public struct ElementStat 
+{
+	public ElementType type;
+	public int value;
+}
+
+public struct BuffStat
+{
+	public ElementType type;
+	public float value;
+}
+
+public class EnemyBase : MonoBehaviour
+{
+	#region Properties
+
+	int health;
+	public int Health
+	{
+		get { return health; }
+		set { health += value; }
+	}
+
+	int strength;
+	public int Strength
+	{
+		get { return strength; }
+		set { strength += value; }
+	}
+
+	ElementStat elementAttack;
+	public ElementStat ElementAttack
+	{
+		get { return elementAttack; }
+		set { elementAttack = value; }
+	}
+
+	// value should be from 1.0-2.0 (see take damage)
+	BuffStat weakness;
+	public BuffStat Weakness
+	{
+		get { return weakness; }
+		set { weakness = value; }
+	}
+
+	// value should be from 0.0-1.0 (see take damage)
+	BuffStat resistance;
+	public BuffStat Resistance
+	{
+		get { return resistance; }
+		set { resistance = value; }
+	}
+
+	#endregion
+
+	public virtual void TakeDamage(ElementStat damage)
+	{
+		float finalDamage = damage.value;
+		switch(damage.type)
+		{
+		case ElementType.Fire:
+			if (resistance.type == ElementType.Fire) finalDamage *= resistance.value;
+			if (weakness.type == ElementType.Fire) finalDamage *= weakness.value;
+			break;
+		case ElementType.Water:
+			if (resistance.type == ElementType.Water) finalDamage *= resistance.value;
+			if (weakness.type == ElementType.Water) finalDamage *= weakness.value;
+			break;
+		case ElementType.Magic:
+			if (resistance.type == ElementType.Magic) finalDamage *= resistance.value;
+			if (weakness.type == ElementType.Magic) finalDamage *= weakness.value;
+			break;
+		}
+
+		// check for complete damage resistance
+		if (finalDamage > 0) health -= (int)finalDamage;
+	}
+
+	public virtual ElementStat GetAttack()
+	{
+		ElementStat attack = elementAttack;
+		attack.value += strength;
+		return attack;
+	}
+}
