9d47d92718adaf84d7952b8db6b4625356b046e8
sookool99 <shawnmacburnie@gmail.com>
2015-01-23 21:42:31 -0400

    created enamyrange and rangeWeapon scripts.

---
+++ GlobalGameJam/Assets/shawn/EnemyRange.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyRange : EnemyBase 
+{
+
+	public void Move()
+	{
+		//move to player if in view range of that player
+		//attack if within attackRange of that player
+	}
+
+
+	void Start () 
+	{
+		Health = 10;
+		Strength = 10;
+		ElementStat stats;
+
+		stats.type = ElementType.Fire;
+		stats.value = 2;
+		ElementAttack = stats;
+
+		BuffStat buff;
+		buff.value = 1.2f;
+		buff.type = ElementType.Water;
+		Weakness = buff;
+
+		buff.type = ElementType.Magic;
+		buff.value = 0.2f;
+		Resistance = buff;
+
+		ViewRange = 10f;
+		AttackRange = 5f;
+	}
+
+	void FixedUpdate () 
+	{
+		
+	}
+
+	public override void TakeDamage(ElementStat damage)
+	{
+		//do take damage animation
+
+		base.TakeDamage(damage);
+	}
+
+
+	public override ElementStat GetAttack ()
+	{
+		//Do attack animation
+
+		return base.GetAttack ();
+	}
+}
