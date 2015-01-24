bd53dbc20b2feb484ca35ea0d6b0ce62c4741477
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-01-24 19:46:37 -0400

    Working on melee enemy movement

--- GlobalGameJam/Assets/Scripts/EnemyMelee.cs
+++ GlobalGameJam/Assets/Scripts/EnemyMelee.cs
@@ -16,8 +16,15 @@ public class EnemyMelee : EnemyBase
 	[Range(1.0f, 2.0f)]
 	public float weaknessMultiplier;
 
-	RaycastHit2D[] hits = new RaycastHit2D[3];
+	[Header("Send Rays From")]
+	public Transform leftRay;
+	public Transform rightRay;
+
+	RaycastHit2D[] hits = new RaycastHit2D[2];
 	bool playerInRange;
+	GameObject player;
+	Vector2 currentDirection;
+	public Animator anim;
 
 	void Start()
 	{
@@ -58,31 +65,20 @@ public class EnemyMelee : EnemyBase
 	void FixedUpdate()
 	{
 		Move();
-	}
-
-	public void Move()
-	{
-		playerInRange = false;
-		// Create raycasts to see if player is in line of sight
-		hits[0] = Physics2D.Raycast(transform.position, Vector2.right, ViewRange);
-		hits[1] = Physics2D.Raycast(transform.position, -Vector2.right, ViewRange);
-		hits[2] = Physics2D.Raycast(transform.position, Vector2.up, ViewRange);
-		foreach (RaycastHit2D h in hits)
-		{
-			if (playerInRange) break;
-			if (h.collider != null)
-			{
-				if (h.collider.CompareTag("Player"))
-				{
-					playerInRange = true;
-				}
-			}
-		}
-
 		// If player in sight
 		if (playerInRange)
 		{
-			// move towards player
+			if (currentDirection == Vector2.right)
+			{
+				anim.SetBool("WalkRight", true);
+				anim.SetBool("WalkLeft", false);
+			} 
+			else if (currentDirection == -Vector2.right)
+			{
+				anim.SetBool("WalkLeft", true);
+				anim.SetBool("WalkRight", false);
+			}
+			transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime);
 		}
 		else
 		{
@@ -91,33 +87,61 @@ public class EnemyMelee : EnemyBase
 		}
 	}
 
-	public override ElementStat GetAttack()
+	public void Move()
 	{
-		ElementStat attack = base.GetAttack();
+		playerInRange = false;
+		// Create raycasts to see if player is in line of sight
+		hits[0] = Physics2D.Raycast(rightRay.position, Vector2.right, ViewRange);
+		Debug.DrawRay(rightRay.position, Vector2.right * ViewRange);
+		Debug.DrawRay(leftRay.position, -Vector2.right * ViewRange);
+		hits[1] = Physics2D.Raycast(leftRay.position, -Vector2.right, ViewRange);
 
-		return attack;
-	}
-
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.CompareTag("Player"))
+		for (int i = 0; i < hits.Length; i++)
 		{
-			// Attack!
-				// attackinfo = GetAttack();
-				// player.takeDamage(attackinfo);
+			if (playerInRange) break;
+			if (hits[i].collider != null)
+			{
+				Debug.Log("Something hit" + hits[i].collider.tag);
+				if (hits[i].collider.CompareTag("Player"))
+				{
+					player = hits[i].collider.gameObject;
+					playerInRange = true;
+					if (i == 0) currentDirection = Vector2.right;
+					else currentDirection = -Vector2.right;
+				}
+			}
 		}
+
+
 	}
 
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-		if (collision.gameObject.tag == "Hookshot")
-		{
-			// Get damage from player and fix below
-			ElementStat attack;
-			attack.type = ElementType.Fire;
-			attack.value = 1;
-			// Above to be replaced from player
-			TakeDamage(attack);
-		}
-	}
+//	public override ElementStat GetAttack()
+//	{
+//		ElementStat attack = base.GetAttack();
+//
+//		return attack;
+//	}
+
+//	void OnTriggerEnter2D(Collider2D other)
+//	{
+//		if (other.CompareTag("Player"))
+//		{
+//			// Attack!
+//				// attackinfo = GetAttack();
+//				// player.takeDamage(attackinfo);
+//		}
+//	}
+//
+//	void OnCollisionEnter2D(Collision2D collision)
+//	{
+//		if (collision.gameObject.tag == "Hookshot")
+//		{
+//			// Get damage from player and fix below
+//			ElementStat attack;
+//			attack.type = ElementType.Fire;
+//			attack.value = 1;
+//			// Above to be replaced from player
+//			TakeDamage(attack);
+//		}
+//	}
 }
