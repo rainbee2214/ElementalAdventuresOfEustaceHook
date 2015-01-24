c4f0b3fe7d057917c84fd242a0ec05d935a42ec6
rainbee2214 <rainbee2214@gmail.com>
2015-01-24 16:31:09 -0400

    Ranged enemy sprite sheet imported.

---
+++ GlobalGameJam/Assets/HookshotController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HookshotController : MonoBehaviour
+{
+    GameObject head;
+    GameObject tail;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
