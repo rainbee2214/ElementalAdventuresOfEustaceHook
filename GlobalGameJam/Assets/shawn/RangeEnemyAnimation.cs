3251b17ad095a4ec342eebc6c7f00e34bc70a35d
sookool99 <shawnmacburnie@gmail.com>
2015-01-24 19:52:39 -0400

    Added animations for enemy and fireball

---
+++ GlobalGameJam/Assets/shawn/RangeEnemyAnimation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangeEnemyAnimation : MonoBehaviour {
+
+	Animator anim;
+	
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	public void Idle()
+	{
+		anim.SetBool("Attack", false);
+		anim.SetBool("WalkLeft", false);
+		anim.SetBool("WalkRight", false);
+	}
+	
+	public void WalkLeft()
+	{
+		anim.SetBool("Attack", false);
+		anim.SetBool("WalkLeft", true);
+		anim.SetBool("WalkRight", false);
+	}
+	
+	public void WalkRight()
+	{
+		anim.SetBool("Attack", false);
+		anim.SetBool("WalkLeft", false);
+		anim.SetBool("WalkRight", true);
+	}
+
+	public void Attack()
+	{
+		anim.SetBool("Attack", true);
+	}
+
+
+}
