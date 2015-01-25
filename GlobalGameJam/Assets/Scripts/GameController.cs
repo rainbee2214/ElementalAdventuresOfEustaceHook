fb10845b9fd9980ca09de3112d0b348f78a6d443
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-01-25 14:15:19 -0400

    Add missing reference to game controller

--- GlobalGameJam/Assets/Scripts/GameController.cs
+++ GlobalGameJam/Assets/Scripts/GameController.cs
@@ -43,6 +43,7 @@ public class GameController : MonoBehaviour
 
 	void SetupPlayerStats()
 	{
+		playerStats = new PlayerStats();
 		playerStats.Health = startHealth;
 		playerStats.Strength = startStrength;
 		playerStats.Armor = startArmor;
