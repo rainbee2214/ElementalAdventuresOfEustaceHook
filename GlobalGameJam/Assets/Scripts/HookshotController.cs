a1b5be739f3b73d11b8fdbedb7d02ef35f6ec798
rainbee2214 <rainbee2214@gmail.com>
2015-01-24 21:26:29 -0400

    Small fixed.

--- GlobalGameJam/Assets/Scripts/HookshotController.cs
+++ GlobalGameJam/Assets/Scripts/HookshotController.cs
@@ -7,9 +7,10 @@ public class HookshotController : MonoBehaviour
     float offset = 1f;
     GameObject head;
     GameObject tail;
-    int currentLength = 2;
+    public int currentLength = 6;
 
     Animator anim;
+    public bool extend;
 
     void Awake()
     {
@@ -20,11 +21,12 @@ public class HookshotController : MonoBehaviour
 
     void FixedUpdate()
     {
-
+        if (extend) Extend();
     }
 
     void Extend()
     {
+        extend = false;
         ExtendAnimation(currentLength);
     }
 
@@ -35,29 +37,6 @@ public class HookshotController : MonoBehaviour
 
     void ExtendAnimation(int length)
     {
-        switch (length)
-        {
-            case 1: anim.SetTrigger(length);  break;
-            case 2: break;
-            case 3: break;
-            case 4: break;
-            case 5: break;
-            case 6: break;
-            case 7: break;
-            case 8: break;
-            case 9: break;
-            case 10: break;
-            case 11: break;
-            case 12: break;
-            case 13: break;
-            case 14: break;
-            case 15: break;
-            case 16: break;
-            case 17: break;
-            case 18: break;
-            case 19: break;
-            case 20: break;
-            default: break;
-        }
+        anim.SetTrigger(length+"");
     }
 }
