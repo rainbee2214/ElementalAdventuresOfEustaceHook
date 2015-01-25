952bfb84d03fef5f61299e80169a5de8f7de691b
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-01-25 16:47:24 -0400

    Miniboss mostly working, mostly

---
+++ GlobalGameJam/Assets/FireballTravel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballTravel : MonoBehaviour 
+{
+	public float speed;
+	public int direction;
+
+	void Start () 
+	{
+	
+	}
+
+	void FixedUpdate () 
+	{
+		transform.position = Vector3.MoveTowards(transform.position, 
+		                                         new Vector3(1000 * direction, 
+		            										 transform.position.y, 
+		            										 transform.position.z), 
+		                                         Time.deltaTime * speed);
+	}
+}
