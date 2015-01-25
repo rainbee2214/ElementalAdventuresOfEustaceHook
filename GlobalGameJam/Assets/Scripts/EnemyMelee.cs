b7c44c500c069fd116e8f0d42488552090b60971
rainbee2214 <rainbee2214@gmail.com>
2015-01-25 16:43:35 -0400

    Working on GameController.

--- GlobalGameJam/Assets/Scripts/EnemyMelee.cs
+++ GlobalGameJam/Assets/Scripts/EnemyMelee.cs
@@ -28,6 +28,10 @@ public class EnemyMelee : EnemyBase
 	public Animator anim;
 //	CircleCollider2D attackTrigger;
 
+    float attackDelay = 1f;
+    float nextAttackTime;
+    bool isAttacking;
+
 	void Start()
 	{
 		// Setup health, strength
@@ -46,7 +50,7 @@ public class EnemyMelee : EnemyBase
 		
 		// Setup resistance & weakness, ensure weakness is not the same as resistance
 		BuffStat res;
-		res.type = Utility.GetRandomEnum<ElementType>();
+        res.type = ElementType.Magic;//Utility.GetRandomEnum<ElementType>();
 		res.value = resistanceMultiplier;
 		Resistance = res;
 		
@@ -86,19 +90,25 @@ public class EnemyMelee : EnemyBase
 				}
 				transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime);
 			}
-			else
+			else if (playerInAttackRange && !isAttacking)
 			{
-				anim.SetBool("WalkRight", false);
-				anim.SetBool("WalkLeft", false);
-				anim.SetTrigger("Attack");
+                isAttacking = true;
+                nextAttackTime = Time.time + attackDelay;
+
 			}
 		}
-		else
-		{
-			// Patrol
-			// Move to one edge of ground, then move to other
-		}
+
+        if (isAttacking && Time.time > nextAttackTime) Attack();
 	}
+
+    public void Attack()
+    {
+        anim.SetBool("WalkRight", false);
+        anim.SetBool("WalkLeft", false);
+        anim.SetTrigger("Attack");
+        GameController.controller.TakeDamage();
+        isAttacking = false;
+    }
 
 	public void Move()
 	{
@@ -116,7 +126,7 @@ public class EnemyMelee : EnemyBase
 			if (playerInRange) break;
 			if (hits[i].collider != null)
 			{
-				Debug.Log("Something hit" + hits[i].collider.tag);
+				//Debug.Log("Something hit " + hits[i].collider.tag);
 				if (hits[i].collider.CompareTag("Player"))
 				{
 					player = hits[i].collider.gameObject;
@@ -171,14 +181,18 @@ public class EnemyMelee : EnemyBase
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		if (collision.gameObject.tag == "Hookshot")
+		if (collision.gameObject.tag == "Player")
 		{
-			// Get damage from player and fix below
-			ElementStat attack;
-			attack.type = ElementType.Fire;
-			attack.value = 1;
-			// Above to be replaced from player
-			TakeDamage(attack);
+            ElementStat attack;
+            attack.type = ElementType.Fire;
+            attack.value = 1;
+            TakeDamage(attack);
+            Debug.Log(Health);
 		}
+
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
 	}
 }
---
+++ GlobalGameJam/Assets/UIController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text healthText;
+
+    float screenWidth;
+    float screenHeight;
+
+    void Awake()
+    {
+        healthText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (screenWidth != Screen.width || screenHeight != Screen.height) PositionUI();
+        WriteToText();
+    }
+
+    void WriteToText()
+    {
+        healthText.text = "Health: " + GameController.controller.GetHealth();
+    }
+
+    void PositionUI()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+        healthText.rectTransform.position = new Vector3(screenWidth / 2f, screenHeight - screenHeight / 10f, 0);
+    }
+}
