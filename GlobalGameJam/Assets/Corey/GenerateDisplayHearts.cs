75f15b500d390149975539b2b8f7b91bfc126720
coreyb6 <cburke6@hotmail.com>
2015-01-25 14:22:03 -0400

    heartDisplay and hearts, error instantiating

---
+++ GlobalGameJam/Assets/Corey/GenerateDisplayHearts.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenerateDisplayHearts : MonoBehaviour {
+
+    Vector2 outOfView = new Vector2(-1000, -1000);
+    GameObject heart;
+    GameObject emptyHeart;
+    public int poolSize = 10;
+    List<GameObject> objectPool;
+    List<GameObject> objectPool2;
+    public Vector2 HeartDisplayPosition;
+
+    void CreatePool()
+    {
+        heart = Resources.Load("Prefabs/Heart", typeof(GameObject)) as GameObject;
+        objectPool = new List<GameObject>();
+        for (int i = 0; i < poolSize; i++)
+        {
+            objectPool.Add(Instantiate(heart) as GameObject);
+            objectPool[i].transform.parent = transform;
+            objectPool[i].name = "Heart" + i;
+            objectPool[i].SetActive(false);
+        }
+
+        heart = Resources.Load("Prefabs/EmptyHeart", typeof(GameObject)) as GameObject;
+        objectPool2 = new List<GameObject>();
+        for (int i = 0; i < poolSize; i++)
+        {
+            objectPool2.Add(Instantiate(emptyHeart) as GameObject);
+            objectPool2[i].transform.parent = transform;
+            objectPool2[i].name = "EmptyHeart" + i;
+            objectPool2[i].SetActive(false);
+        }
+    }
+
+    float ScreenW;
+    float ScreenH;
+    void DisplayHealth()
+    {
+        ScreenW = Screen.width;
+        ScreenH = Screen.height;
+        HeartDisplayPosition = new Vector2(ScreenW - ScreenW / 10f, ScreenH - ScreenH / 10f);
+        for (int i = 0; i < GameController.controller.playerStats.Health; i++ )
+        {
+            objectPool[i].GetComponent<Heart>().TurnHeartOn(HeartDisplayPosition);
+            HeartDisplayPosition.x += 1.5f;
+        }
+        for (int i = GameController.controller.playerStats.Health; i <= poolSize; i++)
+        {
+            objectPool[i].GetComponent<Heart>().TurnEmptyHeartOn(HeartDisplayPosition);
+            HeartDisplayPosition.x += 1.5f;
+        }
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+	    CreatePool();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        DisplayHealth();
+	}
+}
---
+++ GlobalGameJam/Assets/Corey/Heart.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Heart : MonoBehaviour {
+
+    Vector2 outOfView = new Vector2(-1000, -1000);
+    GameObject heart;
+    GameObject emptyHeart;
+    public int poolSize = 10;
+    List<GameObject> objectPool;
+    List<GameObject> objectPool2;
+    public int current = 0;
+    public GenerateDisplayHearts generateDisplayHearts;
+
+    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            GameController.controller.playerStats.Health += 1;
+            ResetHeart();
+        }
+    }
+
+    public void TurnHeartOn(Vector2 position)
+    {
+        gameObject.SetActive(true);
+        if (position == generateDisplayHearts.HeartDisplayPosition)
+        {
+            gameObject.collider2D.enabled = false;
+        }
+        transform.position = position;
+    }
+
+    public void TurnEmptyHeartOn(Vector2 position)
+    {
+        gameObject.SetActive(true);
+        transform.position = position;
+    }
+
+    public void ResetHeart()
+    {
+        gameObject.SetActive(false);
+        transform.position = outOfView;
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+        generateDisplayHearts = GameObject.FindGameObjectWithTag("DisplayHearts").GetComponent<GenerateDisplayHearts>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+    }
+}
