6c310172b82027b45409a9f44df463a1d5d03adc
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-01-25 12:16:30 -0400

    Work on player stats

--- GlobalGameJam/Assets/Scripts/GameController.cs
+++ GlobalGameJam/Assets/Scripts/GameController.cs
@@ -7,6 +7,16 @@ public class GameController : MonoBehaviour
 
 	public BossController bossController;
 
+	public PlayerStats playerStats;
+
+	[Header("Player's Starting Stats")]
+	public int startHealth;
+	public int startStrength;
+	public int startArmor;
+	public int startFireStat;
+	public int startWaterStat;
+	public int startMagicStat;
+
     void Awake()
     {
         if (controller == null)
@@ -23,10 +33,31 @@ public class GameController : MonoBehaviour
     void Start()
     {
         controller = this;
+		SetupPlayerStats();
     }
 
     void Update()
     {
 
     }
+
+	void SetupPlayerStats()
+	{
+		playerStats.Health = startHealth;
+		playerStats.Strength = startStrength;
+		playerStats.Armor = startArmor;
+
+		ElementStat genericStat;
+		genericStat.type = ElementType.Fire;
+		genericStat.value = startFireStat;
+		playerStats.FireStat = genericStat;
+
+		genericStat.type = ElementType.Water;
+		genericStat.value = startWaterStat;
+		playerStats.WaterStat = genericStat;
+
+		genericStat.type = ElementType.Magic;
+		genericStat.value = startMagicStat;
+		playerStats.MagicStat = genericStat;
+	}
 }
---
+++ GlobalGameJam/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats
+{
+	int health;
+	public int Health
+	{
+		get { return health; }
+		set { health += value; }
+	}
+
+	int strength;
+	public int Strength
+	{
+		get { return strength; }
+		set { strength += value; }
+	}
+
+	int armor;
+	public int Armor
+	{
+		get { return armor; }
+		set { armor += value; }
+	}
+
+	ElementStat fireStat;
+	public ElementStat FireStat
+	{
+		get { return fireStat; }
+		set { fireStat = value; }
+	}
+
+	ElementStat waterStat;
+	public ElementStat WaterStat
+	{
+		get { return waterStat; }
+		set { waterStat = value; }
+	}
+
+	ElementStat magicStat;
+	public ElementStat MagicStat
+	{
+		get { return magicStat; }
+		set { magicStat = value; }
+	}
+
+	int hookRange;
+	public int HookRange
+	{
+		get { return hookRange; }
+		set { hookRange += value; }
+	}
+}
